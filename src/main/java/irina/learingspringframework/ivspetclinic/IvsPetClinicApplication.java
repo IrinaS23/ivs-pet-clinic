af7a037f2c08e8c1903ac5e35bfed3a2572bc98e
irina <irinav.sava@gmail.com>
2022-06-20 22:04:25 +0300

    initial project add

---
+++ src/main/java/irina/learingspringframework/ivspetclinic/IvsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package irina.learingspringframework.ivspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvsPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvsPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/irina/learingspringframework/ivspetclinic/IvsPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package irina.learingspringframework.ivspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvsPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
