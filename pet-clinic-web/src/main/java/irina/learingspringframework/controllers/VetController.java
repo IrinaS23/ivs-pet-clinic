5d1481da5a5db663f83d45e1782c116d307f8e03
irina <irinav.sava@gmail.com>
2022-07-23 13:18:21 +0300

    list vets added

--- pet-clinic-web/src/main/java/irina/learingspringframework/controllers/VetController.java
+++ pet-clinic-web/src/main/java/irina/learingspringframework/controllers/VetController.java
@@ -1,13 +1,30 @@
 package irina.learingspringframework.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import irina.learingspringframework.ivspetclinic.services.VetService;
 
 @Controller
 public class VetController {
 	
-	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
-	public String listsVets() {
+	private final VetService vetService;
+	
+	
+	
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
+
+
+	@RequestMapping(value = {"/vets","/vets/index","/vets/index.html"}, method = RequestMethod.GET)
+	public String listsVets(Model model) {
+		
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 
